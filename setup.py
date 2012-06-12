45a0c67ff2a9c3274768ce0f09653330ecb8b92e
Brian Waldon <bcwaldon@gmail.com>
2012-06-11 20:23:43 -0700

    Add 'envy' wrapper for fab commands
    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='cloudenvy',
+    version='0.0.1',
+    description='',
+    author='',
+    author_email='',
+    install_requires=['fabric'],
+    packages=['cloudenvy'],
+    scripts=['bin/envy'],
+)
