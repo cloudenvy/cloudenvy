7c5008eb2c6e3a8dc88da4fbdf9450884ff2bf33
Vishvananda Ishaya <vishvananda@gmail.com>
2012-07-26 16:37:32 -0700

    Make setup.py more robust
    Adds more information to the config for setup. Adds fallback
    to distutils. Changes the requirements to use pypi instead
    of an egg.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-import setuptools
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
 
-setuptools.setup(
+config = dict(
     name='cloudenvy',
-    version='0.0.1',
-    description='',
-    author='',
-    author_email='',
-    install_requires=['fabric'],
+    version='0.0.2',
+    url='https://github.com/bcwaldon/cloudenvy',
+    description='Fast provisioning on openstack clouds.',
+    author='Brian Waldon',
+    author_email='bcwaldon@gmail.com',
+    install_requires=['fabric', 'python-novaclient'],
     packages=['cloudenvy'],
     scripts=['bin/envy'],
 )
+
+setup(**config)
