8ce908d62a47d20fbb23e0079e0b69e834f4ce2c
Joe Heck <heckj@mac.com>
2012-10-02 12:27:25 -0700

    added decorator to fail and log
    specifically on 400/BadRequest exceptions propogated upward from the
    novaclient. Updated @notfound decorator to be consistent on internal
    methods as well.
    
    fixes #28

--- cloudenvy/cloud.py
+++ cloudenvy/cloud.py
@@ -17,6 +17,23 @@ def not_found(func):
     return wrapped
 
 
+def bad_request(func):
+    """decorator to wrap novaclient functions that may return a
+    400 'BadRequest' exception when the endpoint is unavailable or
+    unable to be resolved.
+    """
+    #novaclient.exceptions.BadRequest
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except novaclient.exceptions.BadRequest:
+            logging.error("Unable to communicate with endpoints: "
+                          "Received 400/Bad Request from OpenStack.")
+            exit()
+    return wrapped
+
+
 class CloudAPI(object):
     def __init__(self, config):
         self._client = None
@@ -42,20 +59,25 @@ class CloudAPI(object):
                 self.auth_url)
         return self._client
 
+    @bad_request
     def list_servers(self):
         return self.client.servers.list()
 
+    @bad_request
     @not_found
     def find_server(self, name):
         return self.client.servers.find(name=name)
 
+    @bad_request
     @not_found
     def get_server(self, server_id):
         return self.client.servers.get(server_id)
 
+    @bad_request
     def create_server(self, *args, **kwargs):
         return self.client.servers.create(*args, **kwargs)
 
+    @bad_request
     def find_free_ip(self):
         fips = self.client.floating_ips.list()
         for fip in fips:
@@ -63,15 +85,18 @@ class CloudAPI(object):
                 return fip.ip
         raise exceptions.NoIPsAvailable()
 
+    @bad_request
     def find_ip(self, server_id):
         fips = self.client.floating_ips.list()
         for fip in fips:
             if fip.instance_id == server_id:
                 return fip.ip
 
+    @bad_request
     def assign_ip(self, server, ip):
         server.add_floating_ip(ip)
 
+    @bad_request
     @not_found
     def find_image(self, name, id=None):
         if id:
@@ -93,39 +118,44 @@ class CloudAPI(object):
                 exit()
         return image
 
+    @bad_request
     @not_found
     def get_image(self, image_id):
         return self.client.images.get(image_id)
 
+    @bad_request
     def snapshot(self, server, name):
         return self.client.servers.create_image(server, name)
 
+    @bad_request
     @not_found
     def find_flavor(self, name):
         return self.client.flavors.find(name=name)
 
+    @bad_request
+    @not_found
     def find_security_group(self, name):
-        try:
-            return self.client.security_groups.find(name=name)
-        except novaclient.exceptions.NotFound:
-            return None
+        return self.client.security_groups.find(name=name)
 
+    @bad_request
+    @not_found
     def create_security_group(self, name):
-        try:
-            return self.client.security_groups.create(name, name)
-        except novaclient.exceptions.NotFound:
-            return None
+        return self.client.security_groups.create(name, name)
 
+    @bad_request
     def create_security_group_rule(self, security_group, rule):
         return self.client.security_group_rules.create(
             security_group.id, *rule)
 
+    @bad_request
     def allocate_floating_ip(self):
         return self.client.floating_ips.create()
 
+    @bad_request
     @not_found
     def find_keypair(self, name):
         return self.client.keypairs.find(name=name)
 
+    @bad_request
     def create_keypair(self, name, key_data):
         return self.client.keypairs.create(name, public_key=key_data)
