dc2254c78d3e98af0de1979c04b196c482519cc0
Brian Waldon <bcwaldon@gmail.com>
2012-11-07 15:31:24 -0800

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def parse_requirements(requirements_filename='requirements.txt'):
 
 config = dict(
     name='cloudenvy',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/cloudenvy/cloudenvy',
     description='Fast provisioning on openstack clouds.',
     author='Brian Waldon',
