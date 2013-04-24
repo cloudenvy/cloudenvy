673d96c8845a69f567ffe4475d5eb9f110b9995c
Brian Waldon <bcwaldon@gmail.com>
2013-04-24 14:10:51 -0700

    Remove --name from 'envy list'

--- cloudenvy/commands/envy_list.py
+++ cloudenvy/commands/envy_list.py
@@ -11,9 +11,6 @@ class EnvyList(object):
         subparser = subparsers.add_parser('list', help=help_str,
                                           description=help_str)
         subparser.set_defaults(func=self.run)
-        subparser.add_argument('-n', '--name', action='store', default='',
-                               help='Specify custom name for an ENVy.')
-
         return subparser
 
     def run(self, config, args):
